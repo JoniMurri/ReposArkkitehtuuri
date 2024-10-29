00e6aa02cddbde5538ef1f81043210028149563c
Joni Muranen <2306093@edu.karelia.fi>
2024-10-29 15:02:34 +0200

    Lisatyy tehtava3

---
+++ App/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using AppLogger;
+
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string text = "APP";
+            string message = "Humanized_Metodin_Tutustuminen";
+            Logger.Log("log metodi: " + text); // Kutsutaan Log-metodia
+
+            Logger.LogHumanized("Humanized metodi: " + message);
+            
+        }
+    }
+}
---
+++ AppLogger/Class1.cs
@@ -0,0 +1,20 @@
+﻿
+using Humanizer;
+
+
+namespace AppLogger;
+
+public class Logger
+{
+
+        public static void Log(string text)
+        {
+            Console.WriteLine(text);
+           
+            
+        }
+        public static void LogHumanized(string message){
+            string humanized = message.Humanize();
+            Console.WriteLine(humanized);
+        }
+}
